4dc71c89d98459504564253ef790f1df0930cdf2
moabs81 <moabs81@hotmail.com>
2017-12-14 17:12:49 -0500

    added json-server, added db.json for fake data, updated eslint config with new rule prefs. busy day.

--- src/app/app.js
+++ src/app/app.js
@@ -8,9 +8,10 @@ import { paintForm } from '../library/book1';
 
 const cbReturn = function(e) {
     e.preventDefault();
+    const ticker = e.srcElement.parentElement.childNodes[2].value;
     const apiRequest = {
         baseURI: 'https://api.iextrading.com/1.0',
-        searchReq: '/stock/' + e.srcElement.parentElement.childNodes[2].value + '/quote',
+        searchReq: '/stock/' + ticker + '/quote',
         method: 'GET',
         success: function() {
             //console.log(this);
@@ -18,9 +19,9 @@ const cbReturn = function(e) {
             if (this.status == 404) {
                 $('#alertBanner').html('404 ERROR - ' + this.statusText);
             } else {
-                var returnObj = JSON.parse(this.response);
+                const returnObj = JSON.parse(this.response);
                 console.log(returnObj);
-                var resultsArr = [
+                const resultsArr = [
                     ['Symbol', returnObj.symbol],
                     ['Company', returnObj.companyName],
                     ['Sector', returnObj.sector],
@@ -34,14 +35,36 @@ const cbReturn = function(e) {
                     '<tr><td id="' + resultsArr[0][1] + '_Cell"> ' + resultsArr[1][1] + ' (' + resultsArr[0][1] + ')</td></tr>'
                 );
                 document.getElementById(resultsArr[0][1] + '_Cell').addEventListener('click', function(e) {
-                    var target = e.srcElement.id.slice(0, -5);
+                    const target = e.srcElement.id.slice(0, -5);
                 });
 
             }
         }
     };
-    apiData.call(apiRequest);
+    (/[A-Za-z]/).test(ticker) ? apiData.call(apiRequest) : console.log('nope, not a valid symbol');
     document.getElementById('tickerInputField').value = '';
     document.getElementById('tickerInputField').focus();
 };
 paintForm.call(cbReturn);
+
+const localAPI = {
+    baseURI: 'http://localhost:3000',
+    searchReq: '/branches?branchName=King',
+    method: 'GET',
+    success: function() {
+        const returnObj = JSON.parse(this.response);
+        console.log(returnObj);
+    }
+};
+
+apiData.call(localAPI);
+
+const myInit = {
+    method: 'GET'
+};
+
+fetch('http://localhost:3000/branches', myInit)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    });
--- src/library/ajaxCalls.js
+++ src/library/ajaxCalls.js
@@ -2,6 +2,7 @@ const apiData = function() {
     const getData = new XMLHttpRequest();
     getData.addEventListener('load', this.success);
     getData.open(this.method, this.baseURI + this.searchReq);
-    (/\/stock\/[A-Za-z]/).test(this.searchReq) ? getData.send() : console.log('nope, not a valid symbol');
+    getData.send();
+    //(/\/stock\/[A-Za-z]/).test(this.searchReq) ? getData.send() : console.log('nope, not a valid symbol');
 };
 export { apiData };
